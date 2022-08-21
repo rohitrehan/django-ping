7d710159b015c515dfd696d59a109ecd0242a27c
Rohit Rehan <git@rohitrehan.com>
2022-08-21 20:00:25 +0530

    removed db sites check

--- ping/defaults.py
+++ ping/defaults.py
@@ -2,8 +2,7 @@ PING_DEFAULT_RESPONSE = "Your site is up!"
 PING_DEFAULT_MIMETYPE = 'text/html'
 
 PING_DEFAULT_CHECKS = (
-    'ping.checks.check_database_sessions',
-    'ping.checks.check_database_sites',
+    'ping.checks.check_database_sessions'
 )
 
 PING_BASIC_AUTH = False
